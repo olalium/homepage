7133ac43222c94fc788645c47c97278cafa9a881
ola <olalium@gmail.com>
2018-09-05 19:07:57 +0200

    added api call which prints to console te response from /api/picturedata

--- src/App/components/pictures/Pictures.js
+++ src/App/components/pictures/Pictures.js
@@ -2,6 +2,20 @@ import React, {Component} from 'react'
 import NavBar from "../navbar/NavBar";
 
 class Pictures extends Component {
+    constructor() {
+        super();
+        this.state = {
+            pictures: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch('/api/picturedata')
+            .then(results => {
+                console.log("data", results.json());
+            });
+    }
+
     render() {
         return(
             <div className="App">
